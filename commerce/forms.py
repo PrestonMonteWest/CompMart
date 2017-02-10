f5456d7e4d941196fa71fd434b8a28abae83191b
prestonmontewest <prestonmontewest@gmail.com>
2017-02-09 18:27:15 -0600

    Got login template working

--- commerce/forms.py
+++ commerce/forms.py
@@ -3,11 +3,3 @@ from django.contrib.auth.models import User
 
 class ProductSearchForm(forms.Form):
     query = forms.CharField(max_length=100)
-
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('username', 'password')
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
