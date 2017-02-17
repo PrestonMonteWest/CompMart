a0d5ed40d12d54601a6a7ebfcae346ead06d259e
prestonmontewest <prestonmontewest@gmail.com>
2017-02-16 20:59:00 -0600

    Incorporated password-changing views and added account index
    Finished moving login and register over to account
        --still need to move related models over to account
    Made account:index URL available in base template

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,29 @@
 from django.shortcuts import reverse
 from django.views.generic import CreateView
 from django.conf.urls import url
-from django.contrib.auth.views import login, password_change
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
 from . import views
 
 app_name = 'account'
 
 urlpatterns = [
-    url(r'^login/$', login, {'template_name': 'account/login.html'}, name='login'),
-    url(r'^password_change/$',
-        password_change,
-        {'template_name': 'account/password.html'},
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.login, {'template_name': 'account/login.html'}, name='login'),
+    url(
+        r'^password_change/$',
+        auth_views.password_change,
+        {
+            'template_name': 'account/password_change.html',
+            'post_change_redirect': 'account:password_change_done',
+        },
         name='password_change'
+    ),
+    url(
+        r'^password_change_done/$',
+        auth_views.password_change_done,
+        {'template_name': 'account/password_change_done.html'},
+        name='password_change_done'
     ),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^register/$', CreateView.as_view(
--- account/views.py
+++ account/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import reverse, redirect
+from django.shortcuts import reverse, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout
 from django.views.decorators.http import require_safe
 from commerce import get_cart
 
 @require_safe
+@login_required
+def index(request):
+    return render(request, 'account/index.html')
+
+@require_safe
 def logout(request):
     cart = get_cart(request.session)
-    auth.logout(request)
+    auth_logout(request)
     request.session['cart'] = cart
     url = request.GET.get('next', reverse('index'))
     return redirect(url)
