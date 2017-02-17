3825d672e007366512116e7428566f2021a525d5
prestonmontewest <prestonmontewest@gmail.com>
2017-02-17 15:43:23 -0600

    Used password_change view for both updating password and success message
    Removed unnecessary password_change_done template

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse_lazy
 from django.conf.urls import url
 from . import views
 
@@ -17,8 +18,19 @@ urlpatterns = [
         {
             'template_name': 'account/password_change.html',
             'post_change_redirect': 'account:password_change_done',
+            'extra_context': {'success': False},
         },
         name='password_change'
+    ),
+    url(
+        r'^password_change_done/$',
+        views.password_change,
+        {
+            'template_name': 'account/password_change.html',
+            'post_change_redirect': 'account:password_change_done',
+            'extra_context': {'success': True},
+        },
+        name='password_change_done'
     ),
     url(
         r'^password_change_done/$',
