7ea2949c2093d71bb4c12385c6d1c99ac6ad74fe
prestonmontewest <prestonmontewest@gmail.com>
2017-02-25 13:41:26 -0600

    Changed settings to use Argon2 hashing algorithm

--- compmart/settings.py
+++ compmart/settings.py
@@ -123,6 +123,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Password hashing
+# https://docs.djangoproject.com/en/1.10/ref/settings/#password-hashers
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
