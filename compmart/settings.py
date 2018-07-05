316cb28d3498173670f1fccce0c6458826abecfc
Preston West <prestonmontewest@gmail.com>
2018-07-04 20:40:42 -0500

    Removed production specific directories Updated settings.py to reflect new database name (for .gitignore)

--- compmart/settings.py
+++ compmart/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'compmart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'compmart.db'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 }
 
